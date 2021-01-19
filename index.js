06c98d40c75cf589b0d9a1fd4495f0be22fa07aa
DKendle <dina.kendle@gmail.com>
2021-01-19 13:28:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 class Formatter {
   //add static methods here
+ 
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  }
+
+  static sanitize(str){
+    return str.replace(/[^A-Za-z0-9-\s']+/g, '');
+  }
+  static titleize(str){
+   let s = str.split(" ")
+   let notThese = ["the","a","an","but","of","and","for","at","by", "form"]
+   
+   
+  }
 }
